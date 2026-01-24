1e8aed963fdf70846e4b257242a086b8dc5a62df
vjeeen <vjeeen15@gmail.com>
2026-01-25 01:16:55 +0800

    Refactor styles for IdCard component

--- src/components/IdCard.tsx
+++ src/components/IdCard.tsx
@@ -54,7 +54,7 @@ export default function IdCard() {
         </div>
 
         {/* SECTION: ТОГГЛ (Бичиг баримтын мэдээлэл) */}
-        <div className="bg-white px-2 py-2 rounded-2xl shadow-sm border border-gray-100 flex items-center justify-between mb-6">
+        <div className="bg-white px-2 py-2 rounded-xl shadow-sm border border-gray-100 flex items-center justify-between mb-6">
           <div className="flex items-center space-x-2 ">
             <div className="w-10 h-10 bg-gray-100 rounded-xl flex items-center justify-center text-gray-500">
               <svg width="22" height="22" viewBox="0 0 24 24" fill="none">
@@ -136,10 +136,10 @@ export default function IdCard() {
                 </div>
               </div>
             </div>
-            <div className="text-[12px] text-white font-medium bg-blue-600 px-2 py-2 rounded-2xl mb-6">
+            <div className="text-[12px] text-white font-medium bg-blue-600 h-18px w-30 rounded-xm mb-4">
               Лавлагаа авах
             </div>
-            <div className="text-[12px] text-blue-600 font-medium bg-blue-100 px-2 py-2 rounded-sm ">
+            <div className="text-[12px] text-blue-600 font-medium bg-blue-100 px-2 py-2 rounded-xm ">
               Дахин захиалах
             </div>
           </div>
