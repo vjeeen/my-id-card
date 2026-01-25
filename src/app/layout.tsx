cc7b5ff1106753ee4acd3deb68a39d3273f1dae3
vjeeen <vjeeen15@gmail.com>
2026-01-25 19:42:27 +0800

    Add viewport metadata to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ["latin", "cyrillic"] });
 export const metadata: Metadata = {
   title: "Миний үнэмлэх",
   description: "ID Card application",
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0',
 };
 
 export default function RootLayout({
